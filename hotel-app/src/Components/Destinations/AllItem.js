37b6458eca7baba871c4ae9d4d12e1433668052c
github_sados45 <somamebarge@gmail.com>
2022-08-24 14:41:57 +0200

    hotel example

---
+++ hotel-app/src/Components/Destinations/AllItem.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react"
+import Dcards from "./Dcards"
+import "../gallery/Gallery.css"
+import "./Destinations.css"
+import Sdata from "./Sdata"
+
+const AllItem = () => {
+  const [items, setItems] = useState(Sdata)
+  return (
+    <>
+      <section className='gallery desi mtop'>
+        <div className='container'>
+          <div className='content grid'>
+            {items.map((item) => {
+              return <Dcards key={item.id} item={item} />
+            })}
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default AllItem
+
